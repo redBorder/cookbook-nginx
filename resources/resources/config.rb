fc2b48b2877fbd5c7ec89cab18a8804ab5ac282a
Miguel Negron <manegron@redborder.com>
2024-05-29 01:02:53 +0100

    lint resources 2

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -4,7 +4,7 @@
 actions :add, :add_s3, :add_erchef, :add_aioutliers, :configure_certs, :remove, :register, :deregister
 default_action :add
 
-attribute :user, kind_of: String, :default => 'nginx'
+attribute :user, kind_of: String, default: 'nginx'
 attribute :s3_port, kind_of: Integer, default: 9000
 attribute :erchef_port, kind_of: Integer, default: 4443
 attribute :aioutliers_port, kind_of: Integer, default: 39091
