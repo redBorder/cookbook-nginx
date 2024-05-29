1862ff6443893b57025651742903cd5af2df8bbe
Miguel Negron <manegron@redborder.com>
2024-05-29 01:02:20 +0100

    lint solo

--- resources/recipes/configure_solo.rb
+++ resources/recipes/configure_solo.rb
@@ -1,13 +1,9 @@
-#
-# Cookbook Name:: nginx
+# Cookbook:: nginx
 # Recipe:: default
-#
-# Copyright 2017, redborder
-#
-# All rights reserved - Do Not Redistribute
-#
+# Copyright:: 2024, redborder
+# License:: Affero General Public License, Version 3
 
-nginx_config "config" do
-  service_name "s3"
+nginx_config 'config' do
+  service_name 's3'
   action [:add, :configure_certs, :add_s3]
 end
