c8fdcb0c7e26c721ccf7aa59b28807e436105743
Rafael Gomez <rgomez@redborder.com>
2025-11-03 10:25:35 +0000

    Remove action restart

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -68,7 +68,6 @@ action :configure_certs do
       not_if { json_cert.empty? }
       variables(crt: json_cert["#{service_name}_crt"])
       action :create
-      notifies :restart, 'service[nginx]', :delayed
     end
 
     template "/etc/nginx/ssl/#{service_name}.key" do
@@ -81,7 +80,6 @@ action :configure_certs do
       not_if { json_cert.empty? }
       variables(key: json_cert["#{service_name}_key"])
       action :create
-      notifies :restart, 'service[nginx]', :delayed
     end
 
     Chef::Log.info("Nginx cookbook - Certs for service #{service_name} has been processed")
